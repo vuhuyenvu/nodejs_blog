6ca4beb93d0bb0bdd171004b85d6eb6db822587c
vuhuyenvu <89784656+vuhuyenvu@users.noreply.github.com>
2022-01-27 15:57:18 +0100

    static file & SCSS

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars')
-const { extname } = require('path')
+const { extname } = require('path')// dat lai ten
 const app = express()
 const port = 3000
 
+// neu la file tinh se vao public
+app.use(express.static(path.join(__dirname,'public')))
+
+// dung morgan hien thi console log 
 app.use(morgan('combined')) 
 //viet tat .hbs cho handlebars
 app.engine('.hbs', engine({
@@ -13,13 +17,15 @@ app.engine('.hbs', engine({
 }
    
 ));
+// su dung handlebars set cai view
 app.set('view engine', '.hbs');
+// set cai view theo path
 app.set('views', path.join(__dirname, 'resources/views'));
 
 // console.log('PATH' , path.join(__dirname, 'resources/views') )
 
-app.get('/news', function (req, res) {
-    res.render('news')
+app.get('/', function (req, res) {
+    res.render('home')
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
