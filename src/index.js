55fc3517628699d93aadce83f020c6af538282a3
vuhuyenvu <89784656+vuhuyenvu@users.noreply.github.com>
2022-02-02 15:10:08 +0100

    install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
-const path = require('path')
-const express = require('express')
-const morgan = require('morgan')
-const { engine } = require('express-handlebars')
-const { extname } = require('path')// dat lai ten
-const mysql = require('mysql')
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const { extname } = require('path'); // dat lai ten
+const mysql = require('mysql');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-
-const route = require('./routes')
-const connect = require('./routes/connectDatabase')
+const route = require('./routes');
+const connect = require('./routes/connectDatabase');
 // su dung thu vien body-parser - form data - input ra console.log
-app.use(express.urlencoded({
-  extended :true
-}))
-app.use(express.json())
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+app.use(express.json());
 //gui code js
 
 // neu la file tinh se vao public
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname, 'public')));
 
-// dung morgan hien thi console log 
-app.use(morgan('combined')) 
+// dung morgan hien thi console log
+app.use(morgan('combined'));
 //viet tat .hbs cho handlebars
-app.engine('.hbs', engine({
-  extname: '.hbs'
-}
-   
-));
+app.engine(
+    '.hbs',
+    engine({
+        extname: '.hbs',
+    }),
+);
 // su dung handlebars set cai view
 app.set('view engine', '.hbs');
 // set cai view theo path
@@ -37,9 +39,9 @@ app.set('views', path.join(__dirname, 'resources/views'));
 // console.log('PATH' , path.join(__dirname, 'resources/views') )
 
 //Route init
-      route(app)
-      connect.cnDB()
+      route(app);
+      connect.cnDB();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+    console.log(`Example app listening on port ${port}`);
+});
